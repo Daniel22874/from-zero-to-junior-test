d8b747820d5cf9f32b30572669bf436f802873e5
Daniel Rodionov <danilrodionov8665@gmail.com>
2026-08-01 10:26:46 +0300

    Main page title

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("This programm worked very good and commited and pushed!")
+    print("Git практика - Главная")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
